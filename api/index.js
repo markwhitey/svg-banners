fd40c94efe6862f04fae8a25d6007f5cf0e2c147
Junxun Yin <markjx.yin@gmail.com>
2024-04-25 17:55:36 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,38 @@
+// 引入依赖库
 const svgs = require("../src/svg");
 
-module.exports = (req, res) => {
-  const { type, text1, text2, height, width } = req.query;
+// Netlify Function 的主要处理函数
+exports.handler = async (event, context) => {
+  const { type, text1, text2, height, width } = event.queryStringParameters;
 
-  res.setHeader("Content-Type", "image/svg+xml");
+  // 设置返回的内容类型为 SVG
+  const headers = {
+    "Content-Type": "image/svg+xml"
+  };
 
+  // 默认错误 SVG
   const error_svg = "origin";
 
   let svg;
 
+  // 判断请求的类型是否存在
   if (type in svgs) {
     svg = svgs[type];
   } else {
+    // 如果请求的类型不存在，返回错误信息
     svg = svgs[error_svg];
     console.log(svg("Type not valid", "refer readme for details !!"));
-    return res.send(svg("Type not valid", "refer readme for details !!"));
+    return {
+      statusCode: 400,
+      headers,
+      body: svg("Type not valid", "refer readme for details !!")
+    };
   }
 
-  res.send(svg({ text1, text2, height, width }));
+  // 正确处理并返回结果
+  return {
+    statusCode: 200,
+    headers,
+    body: svg({ text1, text2, height, width })
+  };
 };
